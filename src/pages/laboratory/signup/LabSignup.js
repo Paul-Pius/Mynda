0471154ddc2add47f2cc7bcc12e036f2fe9f0987
yoppeep <paulpius55@gmail.com>
2023-01-16 10:27:50 +0100

    nineth commit

---
+++ src/pages/laboratory/signup/LabSignup.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const LabSignup = () => {
+	return (
+		<>
+		<div className = "h-form-header">
+			<h3>Create Account</h3>
+		</div>
+
+		<div className = "h-header-message">
+			<p>This info is pertaining to you as a primary contact of your laboratory</p>
+		</div>
+		</>
+	)
+}
+
+export default LabSignup;
